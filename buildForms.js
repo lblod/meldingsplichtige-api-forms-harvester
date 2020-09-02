8948c89b372224f913e95f72d1bf01331271cf36
Claire Lovisa <lovisa.claire@gmail.com>
2020-09-02 11:44:19 +0200

    Rename outputFiles folder to formSkeleton

--- buildForms.js
+++ buildForms.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const read = require('fs-readdir-recursive')
 
 //1-READ ALL OUTPUT FILES AND CONCAT THEM
-const outputDirPath = './outputFiles/';
+const outputDirPath = './formSkeleton/';
 
-let output=fs.readFileSync(outputDirPath+"inputfields/input-fields.ttl", "utf8");;
+let output=fs.readFileSync(outputDirPath+"inputFields/input-fields.ttl", "utf8");;
 
 read(outputDirPath+"forms/").forEach((fileDir, i)=>{
   output+=fs.readFileSync(outputDirPath+"forms/"+fileDir, "utf8");
